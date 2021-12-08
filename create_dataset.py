bbf498891affa17687e7612333786f9881d78cff
Christophe Karam <christophe.karam@grenoble-inp.org>
2021-12-08 22:17:39 +0100

    Add argument parser to dataset creation script

--- create_dataset.py
+++ create_dataset.py
@@ -1,30 +1,91 @@
+import argparse
 import numpy as np
 from dataset.utils import create_dataset
 
 
 if __name__ == '__main__':
-    libri_root = 'data/LibriSpeech/dev-clean'
-    seed = 1
-    srate = 16000
 
-    data_dir = 'data/noised_synth_babble'
+    parser = argparse.ArgumentParser(
+        description='Create artificially-noised LibriSpeech dataset.')
 
-    # Dataset Options
-    g_babble_range = np.linspace(0.05, 0.2, 5)
-    g_room_range = np.linspace(0.5, 2, 4)
-    n_samples_babble_range = np.arange(30, 71, 10)
-    n_iters = 1
-    N = 200
-    save_noise = False
-    save_phase = False
+    # Data options
+    parser.add_argument(
+        '-n', '--N',
+        type=int, default=200, required=True,
+        help='number of LibriSpeech indexes to use')
+    parser.add_argument(
+        '-iter', '--n-iters',
+        type=int, default=1,
+        help='number of iterations over each data sample')
+    parser.add_argument(
+        '--seed',
+        type=int, default=0,
+        help='seed value for random number generator')
+    parser.add_argument(
+        '-sr', '--srate',
+        type=int, default=16000,
+        help='audio signal sampling rate')
+    # Data path arguments
+    parser.add_argument(
+        '-i', '--data-root',
+        type=str, default='data/LibriSpeech/dev-clean',
+        help='path to dataset root')
+    parser.add_argument(
+        '-o', '--out-dir',
+        type=str, default='data/noised_synth_babble',
+        help='destination path to created dataset')
+    # Boolean flag arguments
+    parser.add_argument(
+        '--save-phase', action='store_true',
+        help='save original signal phase')
+    parser.add_argument(
+        '--save-noise', action='store_true',
+        help='save original signal spectrogram')
+    # Range arguments
+    parser.add_argument(
+        '-bg', '--babble-gain',
+        type=float, nargs='+',
+        default=[0.05, 0.2, 5.],
+        help='babble gain range, args to np.linspace(lower, upper, n)')
+    parser.add_argument(
+        '-bs', '--babble-samples',
+        type=int, nargs='+',
+        default=[30, 71, 10],
+        help=('number of samples constituting babble range, '
+            'args to np.arange(lower, upper, step)'))
+    parser.add_argument(
+        '-rg', '--room-gain',
+        type=float, nargs='+',
+        default=[0.5, 2., 4],
+        help='room gain range, args to np.linspace(lower, upper, n)')
+
+    args = parser.parse_args()
+
+    # Parse range arguments
+    if (len(args.babble_gain) > 2):
+        g_babble_range = np.linspace(
+            args.babble_gain[0], args.babble_gain[1], int(args.babble_gain[2]))
+    else:
+        g_babble_range = np.linspace(0.05, 0.2, 5)
+
+    if (len(args.room_gain) > 2):
+        g_room_range = np.linspace(
+            args.room_gain[0], args.room_gain[1], int(args.room_gain[2]))
+    else:
+        g_babble_range = np.linspace(0.5, 2, 4)
+
+    if (len(args.room_gain) > 2):
+        n_samples_babble_range = np.arange(*args.babble_samples)
+    else:
+        n_samples_babble_range = np.arange(30, 71, 10)
 
     create_dataset(
-        libri_root, data_dir, N=N,
+        args.data_root, args.out_dir, N=args.N,
         g_babble_range=g_babble_range,
         g_room_range=g_room_range,
         n_samples_babble_range=n_samples_babble_range,
-        n_iters=n_iters,
-        save_noise=save_noise,
-        save_phase=save_phase,
-        seed=seed,
-        srate=srate)
+        n_iters=args.n_iters,
+        save_noise=args.save_noise,
+        save_phase=args.save_phase,
+        seed=args.seed,
+        srate=args.srate)

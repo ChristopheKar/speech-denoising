9672a3a685ade71e95979861f80ea36f64d60cbd
Christophe Karam <christophe.karam@grenoble-inp.org>
2021-12-16 22:27:49 +0100

    Fix bug in spectrogram to mag-phase

--- dataset/audio.py
+++ dataset/audio.py
@@ -13,8 +13,8 @@ def wav_to_mag_phase(waveform, n_fft=512, hop_len=64, win_len=512, window='hammi
         waveform, n_fft=n_fft, hop_length=hop_len,
         win_length=win_len, window=window)
 
-    mag, phase = librosa.magphase(spectrogram)
-    mag_db = librosa.amplitude_to_db(mag, ref=np.max)
+    mag = np.real(spectrogram)
+    phase = np.imag(spectrogram)
 
     return mag, phase
 

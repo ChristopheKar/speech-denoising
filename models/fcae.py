13497ee980708f97f3355cbf857d281c28d44f2c
Christophe Karam <christophe.karam@grenoble-inp.org>
2022-01-16 17:00:36 +0100

    Update FCAE model configuration and layer construction

--- models/fcae.py
+++ models/fcae.py
@@ -5,7 +5,10 @@ from torch import nn
 class FCAE(nn.Module):
     """Implements a basic fully-connected autoencoder network"""
 
-    def __init__(self, in_shape=(256, 256), n_layers=3, z_dim=64, dropout=0.):
+    def __init__(
+        self, in_shape=(256, 256),
+        encoder_dims=[128, 64, 32], decoder_dims=[32, 64, 128],
+        z_dim=16, out_activation='relu', dropout=0.):
         super(FCAE, self).__init__()
 
         self.name = 'Fully Connected Autoencoder'
@@ -16,18 +19,24 @@ class FCAE(nn.Module):
         self.flatten = nn.Flatten()
 
         # Get layer dimensions
-        layer_sizes = self._get_layer_dims(n_layers, z_dim, self.n_input)
+        encoder_dims = [self.n_input] + encoder_dims + [z_dim]
+        decoder_dims = [z_dim] + decoder_dims + [self.n_input]
 
         # Define activation "layers"
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
+        if (out_activation == 'relu'):
+            self.out_activation = self.relu
+        else:
+            self.out_activation = self.sigmoid
+
         # Encoder
         encoder_layers = []
-        for i in range(len(layer_sizes) - 1):
+        for i in range(1, len(encoder_dims)):
             encoder_layers.append(nn.Linear(
-                layer_sizes[-1-i],
-                layer_sizes[-1-i-1]))
+                encoder_dims[i-1],
+                encoder_dims[i]))
             encoder_layers.append(self.relu)
 
         # Insert dropout layer before final encoder layer
@@ -36,37 +45,23 @@ class FCAE(nn.Module):
 
         self.encoder = nn.Sequential(*encoder_layers)
 
-        # Decoder
         decoder_layers = []
-        for i in range(len(layer_sizes) - 1):
+        for i in range(1, len(decoder_dims)):
             decoder_layers.append(nn.Linear(
-                layer_sizes[i],
-                layer_sizes[i+1]))
+                decoder_dims[i-1],
+                decoder_dims[i]))
             decoder_layers.append(self.relu)
 
+        decoder_layers[-1] = self.out_activation
         self.decoder = nn.Sequential(*decoder_layers)
 
 
     def __repr__(self):
-        return self.name + '()'
+        return self.name
+
 
     def __str__(self):
-        return self.name + '()'
-
-
-    def _get_layer_dims(self, n_layers, z_dim, n_input):
-        z_coef = self._nearest_2pow_coef(z_dim) + 1
-        conv_sizes = [2**i for i in range(z_coef, z_coef + n_layers - 1)]
-        conv_sizes = [z_dim] + conv_sizes + [n_input]
-        return conv_sizes
-
-
-    def _nearest_2pow(self, x):
-        return 2**np.ceil(np.log10(x)/np.log10(2)).astype('int')
-
-
-    def _nearest_2pow_coef(self, x):
-        return np.log2(self._nearest_2pow(x)).astype('int')
+        return self.name
 
 
     def forward(self, x):
@@ -76,7 +71,7 @@ class FCAE(nn.Module):
         # Encoder Chain
         z = self.encoder(x)
         # Decoder Chain
-        x = self.sigmoid(self.decoder(z))
+        x = self.decoder(z)
         # Reshape output
         out = x.reshape((-1, *self.in_shape))
         return out

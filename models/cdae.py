d204f7fcb03bb59c241aff36af21e13fb26eac1a
Christophe Karam <christophe.karam@grenoble-inp.org>
2022-01-16 22:32:09 +0100

    Update CDAE model layers and config

--- models/cdae.py
+++ models/cdae.py
@@ -3,7 +3,10 @@ from torch import nn
 
 
 class CDAE(nn.Module):
-    def __init__(self, n_layers=3, z_dim=8, in_channels=1, batch_norm=False):
+    def __init__(
+        self, in_channels=1,
+        encoder_dims=[128, 64, 32], decoder_dims=[32, 64, 128],
+        z_dim=16, out_activation='sigmoid', batch_norm=False):
         super(CDAE, self).__init__()
 
         self.name = 'Convolutional Autoencoder'
@@ -12,19 +15,22 @@ class CDAE(nn.Module):
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
 
+        if (out_activation == 'relu'):
+            self.out_activation = self.relu
+        else:
+            self.out_activation = self.sigmoid
+
         # Pooling layer
         self.pool = nn.MaxPool2d(2, 2)
 
         # Encoder
-        z_coef = self._nearest_2pow_coef(z_dim) + 1
-        conv_sizes = [2**i for i in range(z_coef, z_coef + n_layers - 1)]
-        conv_sizes = [z_dim] + conv_sizes + [in_channels]
+        encoder_dims = [in_channels] + encoder_dims
         encoder_layers = []
-        for i in range(len(conv_sizes) - 1):
+        for i in range(len(encoder_dims) - 1):
             # Conv layer
             encoder_layers.append(nn.Conv2d(
-                in_channels=conv_sizes[-1-i],
-                out_channels=conv_sizes[-1-i-1],
+                in_channels=encoder_dims[i],
+                out_channels=encoder_dims[i+1],
                 kernel_size=3,
                 padding='same'))
             # Relu activation
@@ -33,43 +39,31 @@ class CDAE(nn.Module):
             encoder_layers.append(self.pool)
             # Batch normalization layer
             if (batch_norm):
-                encoder_layers.append(nn.BatchNorm2d(conv_sizes[-1-i-1]))
+                encoder_layers.append(nn.BatchNorm2d(encoder_dims[i+1]))
 
         self.encoder = nn.Sequential(*encoder_layers)
 
         # Decoder
+        decoder_dims = decoder_dims + [in_channels]
         decoder_layers = []
-        for i in range(len(conv_sizes) - 1):
+        for i in range(len(decoder_dims) - 1):
             decoder_layers.append(nn.ConvTranspose2d(
-                in_channels=conv_sizes[i],
-                out_channels=conv_sizes[i+1],
+                in_channels=decoder_dims[i],
+                out_channels=decoder_dims[i+1],
                 kernel_size=2,
                 stride=2))
             decoder_layers.append(self.relu)
 
+        decoder_layers[-1] = self.out_activation
         self.decoder = nn.Sequential(*decoder_layers)
 
 
     def __repr__(self):
-        return self.name + '()'
+        return self.name
+
 
     def __str__(self):
-        return self.name + '()'
-
-
-    def _get_layer_dims(self, n_layers, z_dim, in_channels):
-        z_coef = self._nearest_2pow_coef(z_dim) + 1
-        conv_sizes = [2**i for i in range(z_coef, z_coef + n_layers - 1)]
-        conv_sizes = [z_dim] + conv_sizes + [in_channels]
-        return conv_sizes
-
-
-    def _nearest_2pow(self, x):
-        return 2**np.ceil(np.log10(x)/np.log10(2)).astype('int')
-
-
-    def _nearest_2pow_coef(self, x):
-        return np.log2(self._nearest_2pow(x)).astype('int')
+        return self.name
 
 
     def forward(self, x):
